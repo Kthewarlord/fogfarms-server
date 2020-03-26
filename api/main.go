b991139cf7aee0b2c83422cb6a4f9424c564eeb7
KitaPDev <kita.pairojtanachai@gmail.com>
2020-03-26 19:43:39 +0700

    edit main.go
    removed excess code

--- api/main.go
+++ api/main.go
@@ -1,21 +1,11 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	"log"
-	"os"
-	"time"
-
-	"github.com/labstack/echo"
+	"github.com/labstack/gommon/log"
 	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
-
-	_articleHttpDeliver "github.com/bxcodec/go-clean-arch/article/delivery/http"
-	_articleRepo "github.com/bxcodec/go-clean-arch/article/repository"
-	_articleUsecase "github.com/bxcodec/go-clean-arch/article/usecase"
-	_authorRepo "github.com/bxcodec/go-clean-arch/author/repository"
-	"github.com/bxcodec/go-clean-arch/middleware"
+	"os"
 )
 
 func init() {
@@ -31,46 +21,12 @@ func init() {
 }
 
 func main() {
-	dbConn := connectDatabase()
-
-	e := echo.New()
-	mdw := middleware.InitMiddleware()
-	e.Use(mdw.CORS)
-	authorRepo := _authorRepo.NewMysqlAuthorRepository(dbConn)
-	ar := _articleRepo.NewMysqlArticleRepository(dbConn)
-
-	timeoutContext := time.Duration(viper.GetInt("context.timeout")) * time.Second
-	au := _articleUsecase.NewArticleUsecase(ar, authorRepo, timeoutContext)
-	_articleHttpDeliver.NewArticleHandler(e, au)
-
-	log.Fatal(e.Start(viper.GetString("server.address")))
-}
-
-func connectDatabase() *sql.DB {
-	dbHost := viper.GetString(`database.host`)
-	dbPort := viper.GetInt(`database.port`)
-	dbUser := viper.GetString(`database.user`)
-	dbPass := viper.GetString(`database.pass`)
-	dbName := viper.GetString(`database.name`)
-	pgConnString := fmt.Sprintf("port=%d host=%s user=%s "+"password=%s dbname=%s sslmode=disable",
-		dbPort, dbHost, dbUser, dbPass, dbName)
-
-	dbConn, err := sql.Open(`postgres`, pgConnString)
-	if err != nil && viper.GetBool("debug") {
-		fmt.Println(err)
-	}
-	err = dbConn.Ping()
-	if err != nil {
-		log.Fatal(err)
+	if err := run(); err != nil {
+		log.Fatal(os.Stderr, "%s\n", err)
 		os.Exit(1)
 	}
-	defer func() {
-		err := dbConn.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
-	log.Println("Database connection successful.")
+}
 
-	return dbConn
+func run() error {
+
 }
