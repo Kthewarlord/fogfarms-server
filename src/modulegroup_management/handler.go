da49b8bf2364509b83caecb31fda904255beccc5
ddfsdd <vchanaichon@windowslive.com>
2020-03-30 11:49:23 +0700

    testingOutBranch

--- src/modulegroup_management/handler.go
+++ src/modulegroup_management/handler.go
@@ -8,6 +8,7 @@ import (
 func MakeHTTPHandler() http.Handler {
 	router := httprouter.New()
 	router.HandlerFunc("GET", "/modulegroup_management", getAllModuleGroup)
+	router.HandlerFunc("GET", "/modulegroup_management/demoJson", getDemoJson)
 
 	return router
 }
--- src/modulegroup_management/services.go
+++ src/modulegroup_management/services.go
@@ -3,13 +3,22 @@ package modulegroup_management
 import(
 	"fmt"
 	"net/http"
+	"github.com/KitaPDev/fogfarms-server/tree/demoReturn"
+	"encoding/json"
 )
 type Services interface {
 	getAllModuleGroup(w http.ResponseWriter, r *http.Request) {
 }
 
-
-
 func getAllModuleGroup(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World!")
+}
+
+type DemoJson struct {
+	DempName    string
+	DemoArray []string
+  }
+
+func getDemoJson(w http.ResponseWriter, r *http.Request){
+	demoJson := DemoJson{"Name", []string{"item 1","item 2"}}
 }
