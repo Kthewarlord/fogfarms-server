da45a6a2c07caa3baba59bba84a361137aadb6ac
ddfsdd <vchanaichon@windowslive.com>
2020-04-25 19:11:53 +0700

    fix assign sslmode

--- src/database/database.go
+++ src/database/database.go
@@ -36,12 +36,14 @@ func GetDB() *sql.DB {
 	var DbUser string
 	var DbName string
 	var port = os.Getenv("PORT")
+	var SSLMODE string
 	if port != "" {
 		DbPass = os.Getenv("DB_PASS")
 		DbPort, err = strconv.Atoi(os.Getenv("DB_PORT"))
 		DbHost = os.Getenv("DB_Host")
 		DbUser = os.Getenv("DB_USER")
 		DbName = os.Getenv("DB_NAME")
+		SSLMODE = "require"
 
 	} else {
 		DbHost = "localhost"
@@ -49,12 +51,13 @@ func GetDB() *sql.DB {
 		DbUser = "postgres"
 		DbPass = "postgres"
 		DbName = "fogfarms-01"
+		SSLMODE = "disable"
 	}
 
 	if db == nil {
 		connectionString := fmt.Sprintf("port=%d host=%s user=%s "+
-			"password=%s dbname=%s",
-			DbPort, DbHost, DbUser, DbPass, DbName)
+			"password=%s dbname=%s sslmode=%s",
+			DbPort, DbHost, DbUser, DbPass, DbName, SSLMODE)
 
 		db, err = sql.Open("postgres", connectionString)
 		if err != nil {
