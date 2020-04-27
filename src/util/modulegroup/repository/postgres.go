b691746f6b7ed07f0779d653a8a6044bc78a1822
ddfsdd <vchanaichon@windowslive.com>
2020-04-27 20:41:42 +0700

    Fix 0 in modulegroupid

--- src/util/modulegroup/repository/postgres.go
+++ src/util/modulegroup/repository/postgres.go
@@ -14,7 +14,7 @@ func GetAllModuleGroups() ([]models.ModuleGroup, error) {
 
 	sqlStatement :=
 		`SELECT ModuleGroupID, ModuleGroupLabel, PlantID, Param_TDs, Param_PH, 
-		Param_Humidity, LightsOnHour, LightsOffHour, TimerLastReset FROM ModuleGroup where modulegroupid!=0;`
+		Param_Humidity, LightsOnHour, LightsOffHour, TimerLastReset FROM ModuleGroup where modulegroupid;`
 
 	rows, err := db.Query(sqlStatement)
 	if err != nil {
